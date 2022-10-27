108cb72c4f4d1731f08efbc2cdd0311254a17b9f
Hana Hrustic <hana.hrustic@gmail.com>
2022-10-27 13:57:50 +0200

    backend folder add with spring initializer

---
+++ backend/ppmApp/src/main/java/com/hhrustic/ppmapp/PpmAppApplication.java
@@ -0,0 +1,13 @@
+package com.hhrustic.ppmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmAppApplication.class, args);
+    }
+
+}
---
+++ backend/ppmApp/src/test/java/com/hhrustic/ppmapp/PpmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhrustic.ppmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
